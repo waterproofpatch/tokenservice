f0ddd2e021440ab90a010fb403a6284666199b84
waterproofpatch <waterproofpatch@gmail.com>
2018-09-30 22:45:54 -0400

    another test

--- test_get_token.py
+++ test_get_token.py
@@ -16,3 +16,21 @@ def test_get_new_token(client):
     assert rv.status == '200 OK'
     assert json.loads(rv.data.decode())['token'] is not None
     assert len(json.loads(rv.data.decode())['token']) == 64
+
+def test_get_unique_tokens(client):
+    """
+    Get two tokens and verify they are unique
+    """
+    rv = client.get('/get_token')
+    assert rv.status == '200 OK'
+    assert json.loads(rv.data.decode())['token'] is not None
+    assert len(json.loads(rv.data.decode())['token']) == 64
+    t1 = json.loads(rv.data.decode())['token']
+
+    rv = client.get('/get_token')
+    assert rv.status == '200 OK'
+    assert json.loads(rv.data.decode())['token'] is not None
+    assert len(json.loads(rv.data.decode())['token']) == 64
+    t2 = json.loads(rv.data.decode())['token']
+
+    assert t1 != t2
